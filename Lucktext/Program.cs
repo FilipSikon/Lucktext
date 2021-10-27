715ba2f0506cb2fbbc0110a57989446d84a1dfa0
Filip Sikon <filip.sikon@elev.ga.ntig.se>
2021-10-27 10:26:58 +0200

    Första

---
+++ Lucktext/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lucktext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
